c48b98a2231ac500dd96ce79c0c68b55748a10c4
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-06-19 16:12:42 +0000

    it is impossible

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt $1 | tee 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt $1 | tee 4-password.txt | grep -oP '(?<=\().*(?=\))'
