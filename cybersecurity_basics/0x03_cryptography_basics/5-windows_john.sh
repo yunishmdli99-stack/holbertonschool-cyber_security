72435eb8d44ee7194deae251d4d69b805346a1e9
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-06-19 16:31:37 +0000

    yea possible

--- cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/5-windows_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=NT --wordlist=/usr/share/wordlists/rockyou.txt $1 | grep -oP '^\S+(?=\s+\()' > 5-password.txt
+john --format=NT --wordlist=/usr/share/wordlists/rockyou.txt $1 > 5-password.txt
