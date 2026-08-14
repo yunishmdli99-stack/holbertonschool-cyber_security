1e240a50f42484ac87a69417a6c1aeac73d36bae
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-08-14 11:37:39 -0400

    N4zy

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 # Identify the IP address responsible for the most requests in a log file
-# Usage: ./dos_finder.sh logs.txt
-
-LOGFILE="$1"
-
-if [ -z "$LOGFILE" ] || [ ! -f "$LOGFILE" ]; then
-    echo "Usage: $0 <logfile>"
-    exit 1
-fi
-
-awk '{print $1}' "$LOGFILE" | sort | uniq -c | sort -rn | head -n 1 | \
-awk '{print "Most requests from IP: " $2 " (" $1 " requests)"}'
+awk '{print $1}' logs.txt | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}'
