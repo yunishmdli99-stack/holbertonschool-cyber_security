b2d5a6b7b00c0cf7d5ed849688196bddc62a51c1
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-08-14 11:55:39 -0400

    3ngl4nd

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -6,6 +6,6 @@ LOGFILE="${1:-logs.txt}"
 # Find the attacker IP (most requests)
 TOP_IP=$(awk '{print $1}' "$LOGFILE" | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}')
 
-# Filter attacker's requests, grab the last quoted field on each line (the User-Agent),
+# Filter attacker's requests, extract User-Agent (6th quote-delimited field),
 # count occurrences, and print the most common one
-grep "^$TOP_IP " "$LOGFILE" | grep -oE '"[^"]*"$' | tr -d '"' | sort | uniq -c | sort -rn | head -n 1 | sed -E 's/^ *[0-9]+ //'
+grep "^$TOP_IP " "$LOGFILE" | awk -F'"' '{print $6}' | sort | uniq -c | sort -rn | head -n 1 | sed -E 's/^ *[0-9]+ //'
